1498ccba83c51c9764ee3e4b6d9ef348375da652
João Matheus Silva Franca <matheussfranca@live.com>
2022-10-03 13:45:07 -0300

    Concluído: Requisito 06

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { description, tag, method,
+      value, coin, exchange, newValue, coinValue } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        <tr>
+          <td>{description}</td>
+          <td>{tag}</td>
+          <td>{method}</td>
+          <td>{value}</td>
+          <td>{coin}</td>
+          <td>{exchange}</td>
+          <td>{newValue}</td>
+          <td>{coinValue}</td>
+        </tr>
+      </table>
     );
   }
 }
+
+Table.propTypes = {
+  description: PropTypes.string,
+  tag: PropTypes.string,
+  method: PropTypes.string,
+  value: PropTypes.string,
+  coin: PropTypes.string,
+  exchange: PropTypes.string,
+  newValue: PropTypes.string,
+  coinValue: PropTypes.string,
+}.isRequired;
 
 export default Table;
